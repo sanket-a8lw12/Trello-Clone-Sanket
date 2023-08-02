83b60bbce65034453d6486d54cf345c654d57830
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-02 14:47:22 +0530

    imported the key and token from .env file and using it

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 import { useState, useEffect } from 'react'
-
 import './App.css'
 import Header from './component/Header'
 import Trello from './component/Trello';
 
 import axios from "axios";
-
 import {
   Route,
   Routes
 } from "react-router-dom";
-
+const { VITE_KEY, VITE_TOKEN } = import.meta.env;
 import TrelloCardList from './component/TrelloCardList';
 
 function App() {
 
   const [trelloData, setTrelloData] = useState([]);
 
-  const baseURL = 'https://api.trello.com/1/members/me/boards?key=7d88baae66e0dbda0675ce6fbb6b1aa8&token=ATTA31f0431674540d74dfa25400a6d53fcd457d38bdb6067c796929effcc92bc0a5B9968DA5';
-
-
+  const baseURL = `https://api.trello.com/1/members/me/boards?key=${VITE_KEY}&token=${VITE_TOKEN}`;
 
   useEffect(() => {
     axios.get(baseURL)
@@ -35,21 +31,15 @@ function App() {
 
 
   async function handleClick(name) {
-    const url = 'https://api.trello.com/1/boards/';
-    const params = {
-      name: name,
-      key: '7d88baae66e0dbda0675ce6fbb6b1aa8',
-      token: 'ATTA31f0431674540d74dfa25400a6d53fcd457d38bdb6067c796929effcc92bc0a5B9968DA5',
-    };
-
-    let newBoard = await axios.post(url, null, { params: params });
+    const url = 'https://api.trello.com/1/boards';
+    let newBoard = await axios.post(`${url}?name=${name}&key=${VITE_KEY}&token=${VITE_TOKEN}`);
     setTrelloData([...trelloData, newBoard])
   }
 
 
   return (
     <>
-    <Header />
+      <Header />
       <Routes>
 
         <Route path="/" element={
@@ -60,7 +50,7 @@ function App() {
         </Route>
         <Route path="/trelloCardList/:id" element={
 
-          <>  
+          <>
             <TrelloCardList />
           </>
         }>
