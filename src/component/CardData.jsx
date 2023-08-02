7faf9c2907a7bad26ac11cc6dfcc08cc57d795db
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-02 18:06:12 +0530

    added the delete function

--- src/component/CardData.jsx
+++ src/component/CardData.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import DeleteIcon from '@mui/icons-material/Delete';
+const { VITE_KEY, VITE_TOKEN } = import.meta.env;
+import axios from "axios";
 
-export default function CardData({name}) {
+export default function CardData({ id, name, setCardData, cardData }) {
+
+  async function deleteCard(){
+    // console.log(id);
+    console.log("clicked on delete")
+    const urlDelete = "https://api.trello.com/1/cards/"
+    let newArr = await axios.delete(`${urlDelete}${id}?key=${VITE_KEY}&token=${VITE_TOKEN}`);
+    console.log(newArr)
+
+    let newArray = cardData.filter((item)=>{
+      // console.log(item.id);
+      // console.log("id = " +id)
+      // console.log("--------")
+      return item.id !== id;
+    })
+    setCardData(newArray);
+    
+  }
+
+  console.log(cardData);
+
+
   return (
-    <div>
-        {name}
-    </div>
-  )
-}
+    <Box sx={{
+      '& > :not(style)': { m: 1 },
+      display: "flex",
+      justifyContent: "space-between",
+      border: "2px solid black",
+    }}>
 
+      <Fab variant="extended" sx={{
+        width: "14em"
+
+      }}>
+        {name}
+      </Fab>
+      <DeleteIcon onClick={() => deleteCard()}
+      sx={{color: "red"}} />
+    </Box>
+  );
+}
 
