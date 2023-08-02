f4a5054d4b765a59cc698c9fe71e9a407458fd76
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-02 14:54:12 +0530

    added the function to add the cardList and fetch the card list

--- src/component/TrelloCardList.jsx
+++ src/component/TrelloCardList.jsx
@@ -1,13 +1,17 @@
-
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import TrelloCard from './TrelloCard';
 import ListCard from './ListCard';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { Card, CardContent, Typography } from '@mui/material';
+const {VITE_KEY, VITE_TOKEN} = import.meta.env;
 
 export default function TrelloCardList() {
 
+
   const [listBoard, setListBoard] = useState([]);
+  const [cardName, setCardName] = useState("");
 
   const { id } = useParams();
 
@@ -24,16 +28,68 @@ export default function TrelloCardList() {
       });
   }, []);
 
+  const cardStyle = {
+    backgroundColor: "lightgrey",
+    // backgroundImage: `url(${cardImage})`,
+    backgroundSize: "cover",
+    color: "white",
+    borderRadius: "0.4em",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    border: "3px solid black",
+    width: "15em",
+    height: "7em",
+  };
+
+
+  const handleCardChange = (name) => {
+    setCardName(name);
+  }
+
+  async function handleAddCard(cardname){
+    const url = `https://api.trello.com/1/boards/${id}/lists?name=${cardname}&key=${VITE_KEY}&token=${VITE_TOKEN}`;
+    let newList = await axios.post(`${url}`);
+    setListBoard([...listBoard, newList]);
+  }
+
   return (
     <div style={{
       display: "flex",
       paddingTop: "5em",
       gap: "1em"
     }}>
+
       {listBoard.map((data) => {
-          return < ListCard key={data.name}
-          name={data.name}/>
-        })}
+        return < ListCard key={data.name}
+          name={data.name} id={data.id}/>
+      })}
+
+      <Card style={cardStyle}>
+        <CardContent >
+          <Typography variant="h6">
+            <TextField sx={{ height: "2em" }}
+              label="Enter List Name"
+              variant="outlined"
+            onChange={(event) => handleCardChange(event.target.value)}
+            />
+          </Typography>
+          <Button onClick={()=> handleAddCard(cardName)}
+            variant="contained"
+            size="medium"
+            sx={{
+              width: '9em',
+              height: "2em",
+              mt: 2,
+              marginLeft: "2.5em"
+            }}>
+
+            Add List
+
+          </Button>
+        </CardContent>
+      </Card>
+
     </div>
   );
 }
