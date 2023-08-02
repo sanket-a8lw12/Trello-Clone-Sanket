82093d967d4fb4cb1cef474aa389e30524549902
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-02 14:49:33 +0530

    wrote the function to fetch the cardlist
    wrote the function to add a card

--- src/component/ListCard.jsx
+++ src/component/ListCard.jsx
@@ -1,30 +1,108 @@
 import { Height, Padding } from '@mui/icons-material';
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { Card, CardContent, Typography } from '@mui/material';
-export default function ListCard({name}) {
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+const { VITE_KEY, VITE_TOKEN } = import.meta.env;
+import axios from "axios";
+import CardData from './CardData';
+
+
+export default function ListCard({ name, id }) {
+
+  const [addACard, setAddACard] = useState("");
+
+  const [cardData, setCardData] = useState([]);
 
 
   const cardStyle = {
     backgroundColor: "grey",
-    // backgroundImage: `url("${image}")`,
     backgroundSize: "cover",
     color: "white",
     borderRadius: "0.4em",
     display: "flex",
     justifyContent: "center",
     border: "3px solid black",
-    width: "14em",
-    height: "6em",
+    width: "15em",
+    flexWrap: "wrap",
   };
+
+
+  function handleAddACardName(name) {
+    setAddACard(name);
+    // console.log(name)
+  }
+
+
+
+  const url = `https://api.trello.com/1/lists/${id}/cards?key=${VITE_KEY}&token=${VITE_TOKEN}`;
+
+  useEffect(() => {
+    axios.get(url)
+      .then((response) => {
+        return response.data
+      }).then((trello) => {
+        setCardData(trello);
+      }).catch((error) => {
+        console.error(error);
+      })
+  }, [])
+
+  async function handleAddACard(name) {
+    const url = 'https://api.trello.com/1/cards';
+    let newCard = await axios.post(`${url}?name=${name}&idList=${id}&key=${VITE_KEY}&token=${VITE_TOKEN}`);
+    setCardData([...cardData, newCard.data])
+    setAddACard('');
+  }
+
   return (
     <>
-        
+
+
+
       <Card style={cardStyle}>
         <CardContent>
-          <Typography variant="h6">
+          <Typography variant="h6" sx={{ textAlign: "center" }}>
             {name}
           </Typography>
+          {cardData.filter((item) => {
+            return item.idList === id;
+          }).map((data) => {
+            console.log(id)
+            console.log("------------------")
+            return <CardData key={data.name} name={data.name} />
+          })
+
+          }
+
+          <Card sx>
+            <CardContent sx={{
+              display: "flex",
+              gap: "1em"
+            }}>
+              <Typography variant="h6">
+                <TextField sx={{
+                  height: "1em",
+                  width: '8em'
+                }}
+                  label="Add a Card"
+                  variant="outlined"
+                  onChange={(event) => handleAddACardName(event.target.value)}
+                />
+              </Typography>
+
+              <Fab size="medium" color="secondary" aria-label="add"
+                onClick={() => handleAddACard(addACard)}
+              >
+                <AddIcon />
+              </Fab>
+            </CardContent>
+          </Card>
+
+
         </CardContent>
       </Card>
     </>
