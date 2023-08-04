dd91aa2cb9959b7883fd81bfde88b486f8fae16a
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-04 13:13:46 +0530

    removed the unnecessary props which was not used

--- src/component/CardData.jsx
+++ src/component/CardData.jsx
@@ -4,28 +4,23 @@ import Fab from '@mui/material/Fab';
 import DeleteIcon from '@mui/icons-material/Delete';
 const { VITE_KEY, VITE_TOKEN } = import.meta.env;
 import axios from "axios";
-import CheckList from './CheckList';
+import CheckListPopUp from './CheckListPopUp';
 
 export default function CardData({ id, name, setCardData, cardData }) {
 
   async function deleteCard(){
-    // console.log(id);
     console.log("clicked on delete")
     const urlDelete = "https://api.trello.com/1/cards/"
     let newArr = await axios.delete(`${urlDelete}${id}?key=${VITE_KEY}&token=${VITE_TOKEN}`);
-    // console.log(newArr)
 
     let newArray = cardData.filter((item)=>{
-      // console.log(item.id);
-      // console.log("id = " +id)
-      // console.log("--------")
       return item.id !== id;
     })
     setCardData(newArray);
     
   }
 
-  console.log(cardData);
+  // console.log(cardData);
 
 
   return (
@@ -38,9 +33,9 @@ export default function CardData({ id, name, setCardData, cardData }) {
 
       <Fab variant="extended" sx={{
         width: "14em"
-
-      }}>
-        <CheckList name={name} id={id}/>
+      }} >
+        {/* {name} */}
+        <CheckListPopUp name={name} id={id}/>
       </Fab>
       <DeleteIcon onClick={() => deleteCard()}
       sx={{color: "red"}} />
