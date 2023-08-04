f08b0a76d138c9a45c6fc16f95dbf26473a7e321
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-04 13:15:13 +0530

    added the new component

--- src/component/CheckList.jsx
+++ src/component/CheckList.jsx
@@ -1,91 +1,28 @@
-// import React from 'react'
-
-// export default function CheckList() {
-//   return (
-//     <div>CheckList</div>
-//   )
-// }
-
-
-import React, {useEffect, useState} from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import CheckBox from './CheckBox';
+import React from 'react'
+import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import axios from "axios"
-const { VITE_KEY, VITE_TOKEN } = import.meta.env;
+import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function CheckList({name, id}) {
-  const [open, setOpen] = React.useState(false);
-  const [list, setList] = useState();
-
-  const listUrl = `https://api.trello.com/1/cards/${id}/checklists?key=${VITE_KEY}&token=${VITE_TOKEN}`;
-
-  useEffect(() => {
-    axios.get(listUrl)
-    .then((response) => {
-      return response.data
-    }).then((listData) => {
-      setList(listData);
-    }).catch((error) => {
-      console.error(error);
-    })
-  }, [])
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    console.log("Clicked add")
-    console.log(list);
-  };
-
-
-
+export default function CheckList({name}) {
   return (
-    <div>
-      <Button onClick={handleClickOpen}>{name}</Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        scroll='body'
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id="scroll-dialog-title">{name}</DialogTitle>
-        <DialogContent dividers={scroll === 'paper'}>
-          <DialogContentText
-            id="scroll-dialog-description"
-            tabIndex={-1}
-          >
-            {/* {[...new Array(2)]
-              .map(
-                () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-              )
-              .join('\n')} */}
-
-              < CheckBox list={list}/>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          {/* <Button onClick={handleClose}>Subscribe</Button> */}
-          <Fab color="primary" aria-label="add" onClick={handleClose}>
-        <AddIcon />
-      </Fab>
-          
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+    // <Box sx={{
+    //     '& > :not(style)': { m: 1 },
+    //     display: "flex",
+    //     justifyContent: "space-between",
+    //     border: "2px solid black",
+    //   }}>
+  
+    //     <Fab variant="extended" sx={{
+    //       width: "14em"
+    //     }}>
+    //       {/* <CheckList name={name}/> */}
+    //     </Fab>
+    //     <DeleteIcon 
+    //     sx={{color: "red"}} />
+    //   </Box>
+    <h1></h1>
+  )
 }
+
+
+//onClick={() => deleteCard()}
