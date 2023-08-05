da18769e556ff1d3d1f9292e30f89db3d45d84dc
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-05 10:36:48 +0530

    added the url to update the checkbox state

--- src/component/CheckBox.jsx
+++ src/component/CheckBox.jsx
@@ -17,11 +17,50 @@ async function addCheckBox() {
   const url = 'https://api.trello.com/1/checklists/';
   let newCheckItem = await axios.post(`${url}${item.id}/checkItems?name=${checkBoxName}&key=${VITE_KEY}&token=${VITE_TOKEN}`);
   console.log(newCheckItem);
+  let newList = list.map((data)=>{
+    if(data.id === item.id){
+      data.checkItems = [...data.checkItems, newCheckItem.data];
+    }
+    return data;
+  })
+
+  setList([...newList]);
 
 }
 
 function handleCheckBoxName(name){
   setCheckBoxName(name)
+}
+
+
+//cards/${cardId}/checkItem/${event.target.id}?
+
+
+function handleCheckItemState(event){
+
+  const updateUrl = 'https://api.trello.com/1/'
+  let newCheckItem = axios.put(`${updateUrl}cards/${id}/checkItem/${event.target.id}?&key=${VITE_KEY}&token=${VITE_TOKEN}`, {
+    state: event.target.checked && 'complete' || 'incomplete'
+  });
+  console.log("new check item updated = "  + newCheckItem.data)
+
+  let newUpdatedList = list.map((data)=>{
+    if(data.id === item.id){
+      console.log("data.check = " +data)
+      data.checkItems = data.checkItems.map((checkItemData)=>{
+        if(checkItemData.id === event.target.id){
+          checkItemData.state = event.target.checked && 'complete' || 'incomplete';
+        }
+        return checkItemData;
+      })
+    }
+    return data;
+ 
+  })
+
+  setList([...newUpdatedList]);
+
+
 }
 
   return (
@@ -41,10 +80,13 @@ function handleCheckBoxName(name){
     </Fab>
     </div>
 
-    {item.checkItems.map((checkBoxItem) => {
+    {item && item.checkItems.map((checkBoxItem) => {
+      // console.log(checkBoxItem);
       return (
         <FormGroup key={checkBoxItem.name}>
-          <FormControlLabel control={<Checkbox defaultChecked />} label={checkBoxItem.name} />
+          <FormControlLabel control={<Checkbox id={checkBoxItem.id} checked ={checkBoxItem.state === 'complete'}
+          onChange={handleCheckItemState}
+          />} label={checkBoxItem.name} />
         </FormGroup>
       )
     })}
