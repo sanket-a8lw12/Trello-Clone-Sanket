0c6d075e8ed5276f5aaa6064ee1fdbd4ebe9fde3
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-08-10 11:26:15 +0530

    replaced the useState with useReducer

--- src/component/TrelloCardList.jsx
+++ src/component/TrelloCardList.jsx
@@ -1,30 +1,51 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import ListCard from './ListCard';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { Card, CardContent, Typography } from '@mui/material';
-const {VITE_KEY, VITE_TOKEN} = import.meta.env;
+const { VITE_KEY, VITE_TOKEN } = import.meta.env;
+
+
+
+// const [listBoard, setListBoard] = useState([]);
+// const [cardName, setCardName] = useState("");
+
+const initialState = {
+  listBoard: [],
+  cardName: '',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_LIST_BOARD':
+      return { ...state, listBoard: action.payload };
+    case 'SET_CARD_NAME':
+      return { ...state, cardName: action.payload };
+    default:
+      return state;
+  }
+}
 
 export default function TrelloCardList() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-
-  const [listBoard, setListBoard] = useState([]);
-  const [cardName, setCardName] = useState("");
 
   const { id } = useParams();
+
+  console.log("useParams = " + id)
 
   const url = `https://api.trello.com/1/boards/${id}/lists?key=7d88baae66e0dbda0675ce6fbb6b1aa8&token=ATTA31f0431674540d74dfa25400a6d53fcd457d38bdb6067c796929effcc92bc0a5B9968DA5`;
 
   useEffect(() => {
     axios.get(url)
       .then((response) => {
-        // console.log(response.data)
         return response.data;
       })
       .then((boardList) => {
-        setListBoard(boardList);
+        // setListBoard(boardList);
+        dispatch({ type: 'SET_LIST_BOARD', payload: boardList });
       });
   }, []);
 
@@ -44,13 +65,15 @@ export default function TrelloCardList() {
 
 
   const handleCardChange = (name) => {
-    setCardName(name);
+    // setCardName(name);
+    dispatch({ type: 'SET_CARD_NAME', payload: name });
   }
 
-  async function handleAddCard(cardname){
+  async function handleAddCard(cardname) {
     const url = `https://api.trello.com/1/boards/${id}/lists?name=${cardname}&key=${VITE_KEY}&token=${VITE_TOKEN}`;
     let newList = await axios.post(`${url}`);
-    setListBoard([...listBoard, newList]);
+    // setListBoard([...listBoard, newList]);
+    dispatch({ type: 'SET_LIST_BOARD', payload: [...state.listBoard, newList] });
   }
 
   return (
@@ -62,9 +85,9 @@ export default function TrelloCardList() {
       height: "85vh"
     }}>
 
-      {listBoard.map((data) => {
-        return < ListCard key={data.name}
-          name={data.name} id={data.id}/>
+      {state.listBoard.map((data) => {
+        return < ListCard key={data.id}
+          name={data.name} id={data.id} />
       })}
 
       <Card style={cardStyle}>
@@ -73,10 +96,10 @@ export default function TrelloCardList() {
             <TextField sx={{ height: "2em" }}
               label="Enter List Name"
               variant="outlined"
-            onChange={(event) => handleCardChange(event.target.value)}
+              onChange={(event) => handleCardChange(event.target.value)}
             />
           </Typography>
-          <Button onClick={()=> handleAddCard(cardName)}
+          <Button onClick={() => handleAddCard(state.cardName)}
             variant="contained"
             size="medium"
             sx={{
